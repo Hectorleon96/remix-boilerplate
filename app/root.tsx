391538b8e6f6b66dd7f4180ac0f348a7c6d8a5af
Hectorleon96 <jobleon.hector@gmail.com>
2025-06-23 22:08:04 -0600

    install tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
+import resetStyles from "~/styles/reset.css?url";
+import tailwindStyles from "~/styles/tailwind.css?url";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,6 +17,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="stylesheet" href={resetStyles}></link>
+        <link rel="stylesheet" href={tailwindStyles}></link>
       </head>
       <body>
         {children}
